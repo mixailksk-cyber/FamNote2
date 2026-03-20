9d835508726b54da6731aca8547635c0b731d332
mixailksk-cyber <mixailksk@gmail.com>
2026-03-20 09:21:43 +0500

    Create MainApplication.kt

---
+++ android/app/src/main/java/com/mkhailksk/famnotes/MainApplication.kt
@@ -0,0 +1,42 @@
+package com.mkhailksk.famnotes
+
+import android.app.Application
+import com.facebook.react.PackageList
+import com.facebook.react.ReactApplication
+import com.facebook.react.ReactNativeHost
+import com.facebook.react.ReactPackage
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
+import com.facebook.react.defaults.DefaultReactNativeHost
+import com.facebook.soloader.SoLoader
+
+class MainApplication : Application(), ReactApplication {
+
+    private val mReactNativeHost = object : DefaultReactNativeHost(this) {
+        override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+
+        override fun getPackages(): List<ReactPackage> {
+            // Список пакетов от Expo
+            val packages = PackageList(this).packages
+            // Добавляем наш виджет-пакет
+            return packages + WidgetPackage()
+        }
+
+        override fun getJSMainModuleName(): String = "index"
+
+        override fun getBundleAssetName(): String? = "index.android.bundle"
+
+        override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+        override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+    }
+
+    override val reactNativeHost: ReactNativeHost
+        get() = mReactNativeHost
+
+    override fun onCreate() {
+        super.onCreate()
+        SoLoader.init(this, false)
+        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+            load()
+        }
+    }
+}
