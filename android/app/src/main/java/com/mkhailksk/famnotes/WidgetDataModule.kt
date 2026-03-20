269e5cec0978a1a1835937e73cbd55560047b252
mixailksk-cyber <mixailksk@gmail.com>
2026-03-20 11:13:30 +0500

    Update WidgetDataModule.kt

--- android/app/src/main/java/com/mkhailksk/famnotes/WidgetDataModule.kt
+++ android/app/src/main/java/com/mkhailksk/famnotes/WidgetDataModule.kt
@@ -3,15 +3,20 @@ package com.mkhailksk.famnotes
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
-import com.facebook.react.bridge.Promise
+import android.util.Log
 import com.mkhailksk.famnotes.widget.NotesWidget
 
 class WidgetDataModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+    
+    private val TAG = "WidgetDataModule"
     
     override fun getName(): String = "WidgetDataModule"
     
     @ReactMethod
     fun updateWidgetNotes(notesJson: String) {
+        Log.d(TAG, "📥 updateWidgetNotes called with JSON length: ${notesJson.length}")
+        Log.d(TAG, "📄 JSON preview: ${notesJson.take(100)}")
+        
         val context = reactApplicationContext
         NotesWidget.updateWidgetNotes(context, notesJson)
     }
