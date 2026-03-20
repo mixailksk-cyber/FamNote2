4977745167d0507b8b38db22e6ad6d0fc751c3e0
mixailksk-cyber <mixailksk@gmail.com>
2026-03-20 09:15:49 +0500

    Create MainActivity.kt

---
+++ android/app/src/main/java/com/mkhailksk/famnotes/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.mkhailksk.famnotes
+
+import android.os.Bundle
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
+
+class MainActivity : ReactActivity() {
+    override fun getMainComponentName(): String = "famnotes"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(null)
+    }
+
+    override fun createReactActivityDelegate(): ReactActivityDelegate =
+        DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+}
