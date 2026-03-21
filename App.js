074baa4dce61f610e4e97faa7bf18d60132d63b5
mixailksk-cyber <mixailksk@gmail.com>
2026-03-22 00:03:48 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import './BL01_Imports';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { ThemeProvider } from './BL21_ThemeContext';
 import AppContent from './BL11_AppContent';
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <AppContent />
+      <ThemeProvider>
+        <AppContent />
+      </ThemeProvider>
     </SafeAreaProvider>
   );
 }
